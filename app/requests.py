a52efd6181b9ddcb48bd92824a12313f059e9449
JoyMarocho <joymarocho@gmail.com>
2022-05-03 19:11:09 +0300

    creating an api request, and api  call, storing base url

---
+++ app/requests.py
@@ -0,0 +1,12 @@
+import requests as rq,json
+from .models import Source,Article
+
+
+#Getting api key
+my_api_key = None
+
+#Getting the news source base url
+base_url = None
+
+#Getting the article url
+article_url = None
---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    NEWS_API_BASE_URL = "https://newsapi.org/v2/top-headlines/sources?apiKey={}"
+    ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    API_KEY= os.environ.get('API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
