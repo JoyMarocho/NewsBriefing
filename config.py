7315dae772f30d2a3bca2c690898541752823d63
JoyMarocho <joymarocho@gmail.com>
2022-05-03 20:48:46 +0300

    creating a Secret Key, creating prodconfig class, and devconfig class

--- config.py
+++ config.py
@@ -3,3 +3,16 @@ class Config:
     ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     API_KEY= os.environ.get('API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
